1a9240cf5dc65c09a7530de5763bfdac2af9715b
Vikvam <vikvam@disroot.org>
2024-09-14 15:23:49 +0200

    Coords bbox (?)

--- src/main_gdal.py
+++ src/main_gdal.py
@@ -1,9 +1,20 @@
 from osgeo import gdal
 import matplotlib.pyplot as plt
+import rasterio
+from rasterio import CRS
+from rasterio.coords import BoundingBox
+from rasterio.warp import transform_bounds
 
 if __name__ == "__main__":
-    dataset = gdal.Open("TCD_2018_010m_cz_03035_v020/DATA/TCD_2018_010m_E44N29_03035_v020.tif", gdal.GA_ReadOnly)
-    band = dataset.GetRasterBand(1)
-    arr = band.ReadAsArray()
-    plt.imshow(arr)
-    plt.show()
+    FILE = "TCD_2018_010m_cz_03035_v020/DATA/TCD_2018_010m_E47N30_03035_v020.tif"
+
+    with rasterio.open(FILE) as src:
+        print(src.width, src.height)
+        print(src.bounds)
+        print(src.crs.data)
+
+        bbox = BoundingBox(*transform_bounds(src.crs, CRS.from_epsg(4326), *src.bounds))
+        print(bbox)
+
+        # window = src.window(*src.bounds)
+        # print(window)
