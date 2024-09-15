75a744d1ab4dabb122bace94cb1e210b791463ed
Vikvam <vikvam@disroot.org>
2024-09-15 12:26:34 +0200

    PARCELY features as dict

--- src/main_shapefiles_geopandas.py
+++ src/main_shapefiles_geopandas.py
@@ -31,30 +31,41 @@ def get_features(gdf, filter):
     return features
 
 
+def get_features_dict(gdf, key):
+    features = {}
+    for idx, row in gdf.iterrows():
+        properties = row.drop("geometry").to_dict()
+        if key in properties.keys():
+            features[properties[key]] = row
+    return features
+
+
 if __name__ == "__main__":
     fig, ax = plt.subplots()
 
     BUD_ID = "59946757010"
     PARCEL_ID = "1284"
 
-    gpd_parcely_def = gpd.read_file("727024/PARCELY_KN_DEF.shp")
+    gpd_parcely_def = gpd.read_file("katastr/727024/PARCELY_KN_DEF.shp")
     # for feature in get_features(gpd_parcely_def, {"DRUPOZ_KOD": lambda property: property == 13, "BUD_ID": lambda property: property is not None}):
     for feature in get_features(gpd_parcely_def, {"DRUPOZ_KOD": lambda property: property == 13, "BUD_ID": lambda property: property == "59946757010"}):
         print(feature)
 
-    gpd_parcely_p = gpd.read_file("727024/PARCELY_KN_P.shp")
-    for feature in get_features(gpd_parcely_p, {"ID": lambda property: property == "1284"}):
-        print(feature)
+    gpd_parcely_p = gpd.read_file("katastr/727024/PARCELY_KN_P.shp")
+    features_parcely_p = get_features_dict(gpd_parcely_p, "ID")
+    print(features_parcely_p["1284"])
+    # for feature in get_features(gpd_parcely_p, {"ID": lambda property: property == "1284"}):
+    #     print(feature)
 
 
-    gpd_budovy_def = gpd.read_file("katastr/723754/BUDOVY_DEF.shp")
-    gpd_budovy_p = gpd.read_file("katastr/723754/BUDOVY_P.shp")
-
-    # BUDOVY DATA:
-    # {'ID': '197', 'ID_2': '59946757010', 'TYPPPD_KOD': '9100101', 'KATUZE_KOD': 723754, 'TEXT_KM': 'bez čp/če', 'SO_KOD': 107329247}
-
-    for feature in get_features(gpd_budovy_def, {"ID_2": lambda property: property == BUD_ID}):
-        print(feature)
-
-    for feature in get_features(gpd_budovy_p, {"ID_2": lambda property: property == BUD_ID}):
-        print(feature)
+    # gpd_budovy_def = gpd.read_file("katastr/723754/BUDOVY_DEF.shp")
+    # gpd_budovy_p = gpd.read_file("katastr/723754/BUDOVY_P.shp")
+    #
+    # # BUDOVY DATA:
+    # # {'ID': '197', 'ID_2': '59946757010', 'TYPPPD_KOD': '9100101', 'KATUZE_KOD': 723754, 'TEXT_KM': 'bez čp/če', 'SO_KOD': 107329247}
+    #
+    # for feature in get_features(gpd_budovy_def, {"ID_2": lambda property: property == BUD_ID}):
+    #     print(feature)
+    #
+    # for feature in get_features(gpd_budovy_p, {"ID_2": lambda property: property == BUD_ID}):
+    #     print(feature)
--- src/potential_coverage_area.py
+++ src/potential_coverage_area.py
@@ -5,6 +5,14 @@ import geopandas as gpd
 import json
 
 def get_avail_tree_area(catastr_folder):
+    def get_features_dict(gdf, key):
+        features = {}
+        for idx, row in gdf.iterrows():
+            properties = row.drop("geometry").to_dict()
+            if key in properties.keys():
+                features[properties[key]] = row
+        return features
+
     def get_features(gdf, filter):
         features = []
         for idx, row in gdf.iterrows():
@@ -19,16 +27,18 @@ def get_avail_tree_area(catastr_folder):
 
     gpd_parcely_def = gpd.read_file(f"{catastr_folder}/PARCELY_KN_DEF.shp")
     gpd_parcely_poly = gpd.read_file(f"{catastr_folder}/PARCELY_KN_P.shp")
+    features_parcely_poly = get_features_dict(gpd_parcely_poly, "ID")
     return_arr = [] 
 
     for feature in get_features(
         gpd_parcely_def, 
         {"DRUPOZ_KOD": lambda property: property == 15 or property == 11}
     ):
-        feature_P = get_features(gpd_parcely_poly, {"ID" : lambda property: property == feature["ID"]})
+        feature_P = features_parcely_poly[feature["ID"]]
         return_arr.append(feature_P[0]["geometry"])
 
     return return_arr
+
 
 if __name__ == "__main__":
     print(get_avail_tree_area("727024"))
